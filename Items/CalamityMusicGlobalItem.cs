bc7c5874df820bed2d3411b491eaacee96b04b9b
Amber Sienna <sienna@amberino.co.uk>
2020-03-15 22:14:50 +0000

    Music boxes can no longer have a modifier/prefix/forge.

---
+++ Items/CalamityMusicGlobalItem.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace CalamityModMusic.Items
+{
+    class CalamityMusicGlobalItem : GlobalItem
+    {
+
+        public override bool? PrefixChance(Item item, int pre, UnifiedRandom rand)
+        {
+            if (item.modItem != null && item.modItem.mod == CalamityModMusic.Instance)
+            {
+                return false;
+            }
+            return null;
+        }
+    }
+}
