85277b04a125d28990c73d66de670c5caed0a7bf
Ozzatron <spazeglitch@gmail.com>
2021-11-12 11:15:42 -0500

    pedantic cleanup of global item

--- Items/CalamityMusicGlobalItem.cs
+++ Items/CalamityMusicGlobalItem.cs
@@ -4,15 +4,10 @@ using Terraria.Utilities;
 
 namespace CalamityModMusic.Items
 {
-    class CalamityMusicGlobalItem : GlobalItem
-    {
-        public override bool? PrefixChance(Item item, int pre, UnifiedRandom rand)
-        {
-            if (item.modItem != null && item.modItem.mod == CalamityModMusic.Instance)
-            {
-                return false;
-            }
-            return null;
-        }
-    }
+	public class CalamityMusicGlobalItem : GlobalItem
+	{
+		// No items from this mod are capable of receiving reforges.
+		public override bool? PrefixChance(Item item, int pre, UnifiedRandom rand) =>
+			item.modItem != null && item.modItem.mod == CalamityModMusic.Instance ? false : (bool?)null;
+	}
 }
