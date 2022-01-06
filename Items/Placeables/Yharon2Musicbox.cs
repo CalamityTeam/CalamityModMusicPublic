7c6c30d3f1333cfd40def6f3c5d65845683fb532
Ozzatron <spazeglitch@gmail.com>
2022-01-05 22:37:15 -0500

    1 - Infernal Catharsis (Classic) music box is now craftable. The recipe now takes an unset Music Box. 2 - Infernal Catharsis (Rebirth) music box is no longer craftable, because this music now plays in Yharon's first phase. 3 - All Yharon music boxes renamed to be crystal clear when they play in-game.

--- Items/Placeables/Yharon2Musicbox.cs
+++ Items/Placeables/Yharon2Musicbox.cs
@@ -1,43 +1,28 @@
 using Terraria.ModLoader;
-using CalamityModMusic.Tiles;
 
 namespace CalamityModMusic.Items.Placeables
 {
-    public class Yharon2Musicbox : ModItem
-    {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Music Box (Yharon, Rebirth)");
-        }
-
-        public override void SetDefaults()
-        {
-            item.useStyle = 1;
-            item.useTurn = true;
-            item.useAnimation = 15;
-            item.useTime = 10;
-            item.autoReuse = true;
-            item.consumable = true;
-            item.createTile = ModContent.TileType<Tiles.Yharon2Musicbox>();
-            item.width = 24;
-            item.height = 24;
-            item.rare = 4;
-            item.value = 100000;
-            item.accessory = true;
-        }
-
-		public override void AddRecipes()
+	public class Yharon2Musicbox : ModItem
+	{
+		public override void SetStaticDefaults()
 		{
-			Mod calamityMod = ModLoader.GetMod("CalamityMod");
-			if (calamityMod != null)
-			{
-				ModRecipe recipe = new ModRecipe(mod);
-				recipe.AddIngredient(calamityMod.ItemType("DarksunFragment"), 5);
-				recipe.AddIngredient(calamityMod.ItemType("CosmiliteBar"), 3);
-				recipe.SetResult(this);
-				recipe.AddTile(calamityMod.TileType("CosmicAnvil"));
-				recipe.AddRecipe();
-			}
+			DisplayName.SetDefault("Music Box (Yharon)");
 		}
-    }
+
+		public override void SetDefaults()
+		{
+			item.useStyle = 1;
+			item.useTurn = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.autoReuse = true;
+			item.consumable = true;
+			item.createTile = ModContent.TileType<Tiles.Yharon2Musicbox>();
+			item.width = 24;
+			item.height = 24;
+			item.rare = 4;
+			item.value = 100000;
+			item.accessory = true;
+		}
+	}
 }
