49e36b1088ca91620aa4025ba40c99cf27c51aa3
Ozzatron <spazeglitch@gmail.com>
2022-04-02 19:46:26 -0400

    Fix tile code of all music box tiles

--- Tiles/CrabulonMusicbox.cs
+++ Tiles/CrabulonMusicbox.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
 using Terraria.DataStructures;
@@ -17,7 +18,7 @@ namespace CalamityModMusic.Tiles
             TileObjectData.newTile.LavaDeath = false;
             TileObjectData.newTile.DrawYOffset = 2;
             TileObjectData.addTile(Type);
-            disableSmartCursor = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
             ModTranslation name = CreateMapEntryName();
             name.SetDefault("Music Box");
             AddMapEntry(new Color(200, 200, 200), name);
@@ -25,15 +26,15 @@ namespace CalamityModMusic.Tiles
 
         public override void KillMultiTile(int i, int j, int frameX, int frameY)
         {
-            Item.NewItem(i * 16, j * 16, 16, 48, ModContent.ItemType<Items.Placeables.CrabulonMusicbox>());
+            Item.NewItem(new EntitySource_TileBreak(i * 16, j * 16), i * 16, j * 16, 16, 48, ModContent.ItemType<Items.Placeables.CrabulonMusicbox>());
         }
 
         public override void MouseOver(int i, int j)
         {
             Player player = Main.LocalPlayer;
             player.noThrow = 2;
-            player.showItemIcon = true;
-            player.showItemIcon2 = ModContent.ItemType<Items.Placeables.CrabulonMusicbox>();
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<Items.Placeables.CrabulonMusicbox>();
         }
     }
 }
