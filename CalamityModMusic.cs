9efd4fad664606f25e0561464bafdec75aeabef8
Merkalto <mercutio@wylfing.net>
2022-08-13 19:57:22 -0500

    WikiThis Music Mod support

--- CalamityModMusic.cs
+++ CalamityModMusic.cs
@@ -1,11 +1,35 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ModLoader;
 
 namespace CalamityModMusic
 {
     public class CalamityModMusic : Mod
     {
         public static CalamityModMusic Instance;
+
+        public const string CalamityWikiURL = "calamitymod.wiki.gg";
+
+        internal Mod wikithis = null;
+
         public CalamityModMusic() => Instance = this;
-        public override void Unload() => Instance = null;
+
+        public override void Load()
+        {
+            wikithis = null;
+            ModLoader.TryGetMod("Wikithis", out wikithis);
+
+            if (!Main.dedServ)
+            {
+                if (wikithis is not null)
+                    wikithis.Call("AddModURL", this, CalamityWikiURL);
+            }
+
+        }
+
+        public override void Unload()
+        {
+            wikithis = null;
+            Instance = null;
+        }
     }
 }
