8e5eba204b12bc8de09002403a228fd389bc5c9f
Dominic <farfetchedking541@gmail.com>
2022-11-10 22:27:16 -0600

    Crafting recipe for old AEW music box

--- Items/Placeables/AdultEidolonWyrmMusicbox.cs
+++ Items/Placeables/AdultEidolonWyrmMusicbox.cs
@@ -26,5 +26,27 @@ namespace CalamityModMusic.Items.Placeables
             Item.value = 100000;
             Item.accessory = true;
         }
+        public override void AddRecipes()
+        {
+            Mod calamity = CalamityModMusic.Instance.calamity;
+            if (calamity != null)
+            {
+                // If any of these Cal items don't exist or got renamed, don't make a recipe at all
+                try
+                {
+                    CreateRecipe().
+                        AddIngredient(ItemID.MusicBox).
+                        AddIngredient(calamity.Find<ModItem>("Voidstone").Type, 50).
+                        AddIngredient(calamity.Find<ModItem>("Lumenyl").Type, 15).
+                        AddIngredient(calamity.Find<ModItem>("RuinousSoul").Type, 3).
+                        AddTile(TileID.LunarCraftingStation).
+                        Register();
+                }
+                catch
+                {
+                    CalamityModMusic.Instance.Logger.Debug("One of the items or tiles in this recipe got renamed internally. Please report this in the #bugs-read-pins channel of the official Calamity discord server.");
+                }
+            }
+        }
     }
 }
