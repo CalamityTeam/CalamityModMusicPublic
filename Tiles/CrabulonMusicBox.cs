e9e94b8cf0eea494b93194caa64a65e3be0d5444
Altixal <73398287+Altixal@users.noreply.github.com>
2023-05-27 16:53:50 +0700

    regex makes sweeping changes 101 + drop fixes

--- Tiles/CrabulonMusicBox.cs
+++ Tiles/CrabulonMusicBox.cs
@@ -1,41 +1,9 @@
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
-using Terraria.ObjectData;
-using Terraria.DataStructures;
 
 namespace CalamityModMusic.Tiles
 {
     [LegacyName("CrabulonMusicbox")]
-    public class CrabulonMusicBox : ModTile
+    public class CrabulonMusicBox : MusicBoxTile
     {
-        public override void SetStaticDefaults()
-        {
-            Main.tileFrameImportant[Type] = true;
-            Main.tileObsidianKill[Type] = true;
-            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
-            TileObjectData.newTile.Origin = new Point16(0, 1);
-            TileObjectData.newTile.LavaDeath = false;
-            TileObjectData.newTile.DrawYOffset = 2;
-            TileObjectData.addTile(Type);
-            TileID.Sets.DisableSmartCursor[Type] = true;
-            
-            AddMapEntry(new Color(191, 142, 111), Language.GetText("ItemName.MusicBox"));
-        }
-
-        public override void MouseOver(int i, int j)
-        {
-            Player player = Main.LocalPlayer;
-            player.noThrow = 2;
-            player.cursorItemIconEnabled = true;
-            player.cursorItemIconID = ModContent.ItemType<Items.Placeables.CrabulonMusicBox>();
-        }
-		
-		public override bool CreateDust(int i, int j, ref int type)
-		{
-			return false;
-		}
     }
 }
---
+++ Tiles/MusicBoxTile.cs
@@ -0,0 +1,62 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace CalamityModMusic.Tiles
+{
+    public abstract class MusicBoxTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.newTile.StyleLineSkip = 2;
+            TileObjectData.addTile(Type);
+            TileID.Sets.DisableSmartCursor[Type] = true;
+
+            AddMapEntry(new Color(191, 142, 111), Language.GetText("ItemName.MusicBox"));
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = TileLoader.GetItemDropFromTypeAndStyle(Type);
+        }
+		
+		public override bool CreateDust(int i, int j, ref int type) => false;
+
+        public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+        {
+            if (Main.gamePaused || !Main.instance.IsActive || Lighting.UpdateEveryFrame && !Main.rand.NextBool(4))
+                return;
+
+            Tile tile = Main.tile[i, j];
+            if (tile.TileFrameX == 36 && tile.TileFrameY % 36 == 0 && (int)Main.timeForVisualEffects % 7 == 0 && Main.rand.NextBool(3))
+            {
+                int goreType = Main.rand.Next(570, 573);
+                Vector2 position = new Vector2(i * 16 + 8, j * 16 - 8);
+                Vector2 velocity = new Vector2(Main.WindForVisuals * 2f, -0.5f);
+                velocity.X *= 1f + Main.rand.NextFloat(-0.5f, 0.5f);
+                velocity.Y *= 1f + Main.rand.NextFloat(-0.5f, 0.5f);
+                if (goreType == 572)
+                    position.X -= 8f;
+
+                if (goreType == 571)
+                    position.X -= 4f;
+
+                Gore.NewGore(new EntitySource_TileUpdate(i, j), position, velocity, goreType, 0.8f);
+            }
+        }
+    }
+}
